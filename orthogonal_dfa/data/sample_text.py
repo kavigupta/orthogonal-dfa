4ddfd1155bddbef6f4a7741871bb2ba96f64bdd7
Kavi Gupta <kavi@berkeley.edu>
2025-09-24 14:53:10 -0700

    simplify and clean up

--- orthogonal_dfa/data/sample_text.py
+++ orthogonal_dfa/data/sample_text.py
@@ -1,16 +1,18 @@
 import numpy as np
 
-from .exon import text
+from .exon import RawExon
 
 
-def sample_text(seed, count):
-    length = len(text)
-    random = np.random.RandomState(seed).choice(4, size=(count, length - 410))
+def sample_text(exon: RawExon, seed, count):
+    trim_zone = exon.cl // 2 + 2
+    length = len(exon.text)
+    assert length > trim_zone * 2
+    random = np.random.RandomState(seed).choice(4, size=(count, length - trim_zone * 2))
     arr = np.concatenate(
         [
-            np.repeat(np.array(text[:202])[None], count, axis=0),
+            np.repeat(np.array(exon.text[:trim_zone])[None], count, axis=0),
             random,
-            np.repeat(np.array(text[-202:])[None], count, axis=0),
+            np.repeat(np.array(exon.text[-trim_zone:])[None], count, axis=0),
         ],
         axis=1,
     )
