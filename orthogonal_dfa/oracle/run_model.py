ad655148234814014c05bd6b3ed3985cb8107fde
Kavi Gupta <kavi@berkeley.edu>
2025-10-01 15:59:18 -0700

    update calibration

--- orthogonal_dfa/oracle/run_model.py
+++ orthogonal_dfa/oracle/run_model.py
@@ -1,5 +1,9 @@
 import torch
 
+from permacache import permacache, stable_hash
+
+from orthogonal_dfa.data.exon import RawExon
+from orthogonal_dfa.data.sample_text import sample_text
 from orthogonal_dfa.math import corr
 
 
@@ -14,10 +18,35 @@ def batched_run(model, arr, batch_size=1024):
     return torch.cat(ys, dim=0)
 
 
-def run_model(model, arr):
+def compute_exon_scores(model, arr):
+    yp = batched_run(model, arr).log_softmax(-1)
+    yp = yp[:, [0, -1], [1, 2]].mean(-1)
+    return yp
+
+
+def run_model(exon, model, arr):
+    calibration = calibrate(exon, model, count=100_000)
     with torch.no_grad():
-        yp = batched_run(model, arr).log_softmax(-1)
-        yp = yp[:, [0, -1], [1, 2]].mean(-1)
-        normalized_target = corr.normalize(yp)
-        hard_target = yp > yp.median()
+        yp = compute_exon_scores(model, arr)
+        normalized_target = (yp - calibration["mean"]) / calibration["std"]
+        hard_target = normalized_target > 0
     return normalized_target, hard_target
+
+
+@permacache("orthogonal_dfa/oracle/evaluate/calibrate")
+def calibrate(exon: RawExon, model, count=100_000):
+    """
+    Returns the fraction of positive predictions on random data.
+
+    :param exon: exon to sample from
+    :param model: model to evaluate
+    :param seed: random seed
+    :param count: number of samples
+    """
+    _, arr = sample_text(exon, int(stable_hash("calibration"), 16), count)
+    with torch.no_grad():
+        yp = compute_exon_scores(model, arr)
+        median = yp.median()
+        mean = yp.mean()
+        std = yp.std()
+    return dict(median=median.item(), mean=mean.item(), std=std.item())
