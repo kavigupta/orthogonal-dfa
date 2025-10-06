b5d12bdee89f2da24fd4363ac6dfcc43709b067a
Kavi Gupta <kavi@berkeley.edu>
2025-10-06 17:54:26 -0400

    regression test: stop codon

--- tests/test_stop_codon.py
+++ tests/test_stop_codon.py
@@ -4,6 +4,7 @@ import frame_alignment_checks as fac
 import numpy as np
 
 from orthogonal_dfa.manual_dfa.stop_codon_dfa import stop_codon_dfa
+from orthogonal_dfa.utils.dfa import hash_dfa
 
 
 class TestStopCodonDFA(unittest.TestCase):
@@ -22,3 +23,15 @@ class TestStopCodonDFA(unittest.TestCase):
         sequences = ["".join(x) for x in np.array(list("ACGT"))[sequences]]
         bad = [dfa.accepts(x) for x in sequences] != is_stop_actual
         self.assertTrue(not any(bad))
+
+    def test_hash_regression_no_orf(self):
+        self.assertEqual(
+            hash_dfa(stop_codon_dfa()),
+            "792296689170f7db5c7627bbcb44b9b62d24b48dc387f2d74b3d99c41c5204e0",
+        )
+
+    def test_hash_regression_no_orf_ta(self):
+        self.assertEqual(
+            hash_dfa(stop_codon_dfa(("TAA", "TGA"))),
+            "98f344f8393d34154ada1870e27dda379401329ce02fe79da7dd3e698f82f65a",
+        )
