6b3de78950579b5b94d696e28c2d9c5e3216bdb9
Kavi Gupta <kavi@berkeley.edu>
2025-10-27 18:56:00 -0700

    cascaded

--- tests/test_probability_fns.py
+++ tests/test_probability_fns.py
@@ -60,6 +60,7 @@ class TestTorchProbabilityFns(unittest.TestCase):
                     [0.9, 0.5],
                     [0.1 * 0.2, 0.5 * 0.8],
                     [0.1 * 0.8 * 1e-7, 0.5 * 0.2 * 1.0],
+                    [0.1 * 0.8 * (1 - 1e-7), 0.5 * 0.2 * 1e-7],
                 ]
             ),
             axis=0,
