d2a4104cd07dce416852249e682be385397a790f
Kavi Gupta <kavi@berkeley.edu>
2025-10-30 18:39:39 -0700

    add script to train splice site DFAs

---
+++ orthogonal_dfa/experiments/splice_sites_train.py
@@ -0,0 +1,50 @@
+import frame_alignment_checks as fac
+from matplotlib import pyplot as plt
+
+from orthogonal_dfa.data.exon import default_exon
+from orthogonal_dfa.manual_dfa.spilce_site_dfa import splice_site_psam_pdfa
+from orthogonal_dfa.psams.train import (
+    identify_first_best_by_validation,
+    train_psam_pdfa_full_learning_curve,
+)
+from orthogonal_dfa.spliceai.load_model import load_spliceai
+
+tolerance = 0.5e-3
+
+
+def trained_splice_site(which, num_epochs):
+    oracle = load_spliceai(400, 0).cuda()
+    ms, meta = train_psam_pdfa_full_learning_curve(
+        default_exon,
+        oracle,
+        splice_site_psam_pdfa(which, 0.5),
+        [],
+        seed=1,
+        epochs=num_epochs,
+        lr=1e-3,
+    )
+    m = ms[identify_first_best_by_validation(meta["val_loss"], tolerance=tolerance)]
+    return m, meta
+
+
+def plot_learning_curve(meta, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    ax.plot(meta["train_loss"], color=fac.plotting.line_color(0), label="Train")
+    ax.plot(
+        meta["val_loss_epochs"],
+        meta["val_loss"],
+        color=fac.plotting.line_color(1),
+        label="Validation",
+    )
+    best_val = identify_first_best_by_validation(meta["val_loss"], tolerance)
+    ax.scatter(
+        meta["val_loss_epochs"][best_val],
+        meta["val_loss"][best_val],
+        color=fac.plotting.line_color(1),
+        label="Selected Model",
+    )
+    ax.set_ylabel("Mutual information [b]")
+    ax.set_xlabel("Epoch")
+    ax.legend()
+    ax.grid(axis="y")
---
+++ orthogonal_dfa/manual_dfa/spilce_site_dfa.py
@@ -0,0 +1,68 @@
+import copy
+from functools import lru_cache
+from typing import Optional, Tuple, Union
+
+import numpy as np
+import torch
+from permacache import stable_hash
+
+from orthogonal_dfa.oracle.evaluate import (
+    conditional_mutual_information_from_log_confusion,
+    evaluate_pdfas,
+)
+from orthogonal_dfa.psams.psam_pdfa import PSAMPDFA
+from orthogonal_dfa.spliceai.best_psams_for_lssi import train_psams_for_splice_site
+from orthogonal_dfa.utils.pdfa import PDFA
+from orthogonal_dfa.utils.probability import ZeroProbability
+
+
+def splice_site_pdfa(num_psams, p_t, zero_prob: ZeroProbability):
+    """
+    Creates a splice site PDFA given the number of PSAMs and the transition probability p_t.
+
+    The PDFA has 2 states: an initial state and an accepting state. The transition from
+    the initial state to the accepting state occurs with probability p_t upon reading
+    a PSAM. If no PSAM is detected, the PDFA remains in the initial state.
+    """
+    initial_state_probs = torch.tensor(
+        [1 - zero_prob.probability, zero_prob.probability]
+    )
+    transition_probs = torch.zeros((2, num_psams + 1, 2)) + zero_prob.probability
+    # 0 transition maps every state to itself with probability 1
+    transition_probs[torch.arange(2), 0, torch.arange(2)] = 1 - zero_prob.probability
+    # 1-num_psams transitions map initial state to accepting state with probability p_t
+    transition_probs[0, 1:, 1] = p_t
+    transition_probs[0, 1:, 0] = 1 - p_t
+    # Accepting state remains accepting
+    transition_probs[1, :, 1] = 1 - zero_prob.probability
+    accepting_state_probs = torch.tensor(
+        [zero_prob.probability, 1 - zero_prob.probability]
+    )
+    return PDFA(
+        torch.logit(initial_state_probs),
+        torch.logit(transition_probs),
+        torch.logit(accepting_state_probs),
+    )
+
+
+def splice_site_psam_pdfa(which, logit_p_t):
+    logit_p_t = torch.clamp(torch.tensor(logit_p_t), -10, 10)
+    p_t = torch.sigmoid(logit_p_t).item()
+    psams = (
+        train_psams_for_splice_site(which, 4, num_batches=100_000, seed=0, lr=3e-4)[0]
+        .eval()
+        .psams
+    )
+    psams = copy.deepcopy(psams).cuda()
+    return PSAMPDFA(
+        psams,
+        splice_site_pdfa(4, p_t, ZeroProbability(1e-7)),
+    ).cuda()
+
+
+def evaluate(exon, oracle, model):
+    return conditional_mutual_information_from_log_confusion(
+        evaluate_pdfas(
+            exon, model, [], oracle, seed=int(stable_hash("evaluate-psam-pdfa"), 16)
+        )
+    )[0]
