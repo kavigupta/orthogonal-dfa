e7a5e1a5261451df10313d4bc7dc2e4a646d705d
Kavi Gupta <kavi@berkeley.edu>
2025-12-17 13:51:07 -0800

    parameterize rnn more

--- orthogonal_dfa/experiments/individual/rnn_gate.py
+++ orthogonal_dfa/experiments/individual/rnn_gate.py
@@ -2,6 +2,7 @@
 import argparse
 
 from orthogonal_dfa.experiments.gate_experiments import train_rnn_direct
+from orthogonal_dfa.module.rnn import LSTMProcessor, RNNProcessor
 
 
 def main():
@@ -13,8 +14,35 @@ def main():
         default=0,
         help="Random seed for training.",
     )
+    parser.add_argument(
+        "--constructor",
+        type=str,
+        choices=["RNNProcessor", "LSTMProcessor"],
+        default="RNNProcessor",
+        help="Type of RNN processor to use.",
+    )
+    parser.add_argument(
+        "--hidden-size",
+        type=int,
+        default=100,
+        help="Hidden size of the RNN.",
+    )
+    parser.add_argument(
+        "--layers",
+        type=int,
+        default=2,
+        help="Number of layers in the RNN.",
+    )
     args = parser.parse_args()
-    train_rnn_direct(args.seed)
+    constructor = dict(RNNProcessor=RNNProcessor, LSTMProcessor=LSTMProcessor)[
+        args.constructor
+    ]
+    train_rnn_direct(
+        args.seed,
+        constructor=constructor,
+        hidden_size=args.hidden_size,
+        layers=args.layers,
+    )
 
 
 if __name__ == "__main__":
