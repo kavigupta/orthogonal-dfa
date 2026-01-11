8bc580eb2afeb0c8001eb25cae62e1ea622681cc
Kavi Gupta <kavi@berkeley.edu>
2026-01-11 07:25:46 -0500

    remove dep

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setuptools.setup(
         "automata-lib==8.3.0",
         "frame-alignment-checks>=0.0.73",
         "coloraide",
-        "pygraphviz",
         "h5py==3.15.1",
         "scikit-learn==1.5.1",
         "permacache==5.0.1",
