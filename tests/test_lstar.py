5d285e900351459d468bc2ae35ae4d8591d20f0a
Kavi Gupta <kavi@berkeley.edu>
2026-01-13 05:10:49 -0500

    two subsequences

--- tests/test_lstar.py
+++ tests/test_lstar.py
@@ -84,6 +84,13 @@ class TestLStar(unittest.TestCase):
         _, dfa, _ = compute_dfa_for_oracle(oracle_creator, accuracy=0.8, seed=0)
         assertDFA(self, dfa, oracle_creator)
 
+    def test_two_subsequences(self):
+        oracle_creator = lambda accuracy, seed: BernoulliRegex(
+            accuracy, seed, regex=r".*1111.*1111.*"
+        )
+        _, dfa, _ = compute_dfa_for_oracle(oracle_creator, accuracy=0.8, seed=0)
+        assertDFA(self, dfa, oracle_creator)
+
     def test_specific_alternation(self):
         oracle_creator = lambda accuracy, seed: BernoulliRegex(
             accuracy, seed, regex=r".*(1111|0000)11.*"
