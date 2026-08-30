b0dafb78516a12f249f86c805e9b9522eef26241
Kavi Gupta <kavi@berkeley.edu>
2026-08-29 23:57:50 -0400

    Drop the prefix count nothing asks for (#255)

--- orthogonal_dfa/l_star/statistics.py
+++ orthogonal_dfa/l_star/statistics.py
@@ -2,7 +2,6 @@ import itertools
 import math
 from typing import Iterator, Optional, Tuple
 
-import numpy as np
 import scipy
 import scipy.special
 
@@ -108,71 +107,6 @@ def evidence_margin_for_population_size(
         if fpr <= acceptable_fpr and fnr <= acceptable_fnr:
             return N, eps
     return None
-
-
-def compute_prefix_set_size(delta, noise_level, acceptable_misclassification):
-    r"""
-    Computes the required number of prefixes to achieve a desired misclassification rate
-    when finding suffixes.
-
-    We conceptualize the process of finding suffixes as follows:
-
-        We have a distribution V over binary strings $2^k$ defined as
-
-        x <- X; v_i <- x_i \oplus B(p)
-
-        We have access to one
-
-        v_0 ~ P(v | x = x_0)
-
-        I want to find a set of elements from P(v | x = x_0) but can only sample from V
-
-    where p is the noise level, and k is the quantity we want to find.
-
-    If we look at hamming distance, we have if v ~ P(v | X=x) that, letting n be the noise vector XORd with the x,
-    we have
-
-    d(v, v_0)
-        := sum_j 1(v[j] ≠ v_0[j])
-        := sum_j 1(x[j] ⊕ n[j] ≠ x_0[j] ⊕ n_0[j])
-        := sum_j 1(x[j] ≠ x_0[j]) ⊕ 1(n[j] ≠ n_0[j])
-
-    The distribution (n_0[j] ≠ n[j]) is Bernoulli with parameter 2p(1-p). Let r = 2p(1-p).
-
-    Let A = d(x, x_0) and B = k - A
-
-    Then, we can split
-
-    d(v, v_0)
-        = sum_{j: x[j] = x_0[j]} 1(n[j] ≠ n_0[j]) + sum_{j: x[j] ≠ x_0[j]} 1(n[j] = n_0[j])
-        = Binomial(B, r) + A - Binomial(A, r)
-        ~= A + Normal(B * r, B * r * (1 - r)) - Normal(A * r, A * r * (1 - r))
-        = A + Normal((B - A) * r, (A + B) * r * (1 - r))
-        = A + Normal((k - 2A) * r, k * r * (1 - r))
-
-    Let delta = A/k. Then, we have
-    d(v, v_0)/k = delta + Normal((1 - 2 delta) * r, r * (1 - r) / k)
-                = delta + r - 2 * delta * r + Normal(0, r * (1 - r) / k)
-
-    We want to bound the probability that d(v', v_0)/k < d(v'', v_0)/k for v' ~ P(v | x = x_0) and v'' ~ P(v | x ≠ x_0).
-
-    d(v', v_0)/k > d(v'', v_0)/k
-    r + Normal(0, r * (1 - r) / k) > delta + r - 2 * delta * r + Normal(0, r * (1 - r) / k)
-    Normal(0, r * (1 - r) / k) > delta - 2 * delta * r + Normal(0, r * (1 - r) / k)
-    Normal(0, r * (1 - r) / k) > delta * (1 - 2 * r) + Normal(0, r * (1 - r) / k)
-    Normal(0, 2 * r * (1 - r) / k) > delta * (1 - 2 * r)
-    Normal(0, 1) > delta * (1 - 2 * r) / sqrt(2 * r * (1 - r) / k)
-
-    Letting z = Φ^{-1}(1 - acceptable_misclassification), we want
-
-    delta * (1 - 2 * r) / sqrt(2 * r * (1 - r) / k) = z
-    delta^2 * (1 - 2 * r)^2 k / (2 * r * (1 - r)) = z^2
-    k  = z^2 (2 * r * (1 - r))  / (delta^2 * (1 - 2 * r)^2)
-    """
-    r = 2 * noise_level * (1 - noise_level)
-    z = scipy.stats.norm.ppf(1 - acceptable_misclassification)
-    k = (z**2 * 2 * r * (1 - r)) / (delta**2 * (1 - 2 * r) ** 2)
-    return int(np.ceil(k))
 
 
 def compute_suffix_size_counterexample_gen(acceptable_misclassification, noise_level):
